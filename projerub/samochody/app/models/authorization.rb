78ee08286479fe854d86fc658229ca9ad5f12834
bwinslawski <wisla162@gmail.com>
2012-12-10 16:48:16 +0100

    czyszczenie

--- projerub/samochody/app/models/authorization.rb
+++
@@ -1,4 +0,0 @@
-class Authorization < ActiveRecord::Base
-  attr_accessible :link, :name, :provider, :secret, :token, :uid, :user_id
-  belongs_to :user
-end
--- projerub/samochody/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
- devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
-  attr_accessible :email, :name, :password, :password_confirmation, :remember_me
-  has_many :authorizations, :dependent => :destroy
-end
